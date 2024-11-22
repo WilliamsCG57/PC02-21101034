ed6c78462af1e04263b5c0ee0eab1b9712436820
Williams Ccompi <ccompiguerrerow@gmail.com>
2024-11-22 00:05:47 -0500

    SortToggle para filtrado de popularidad

---
+++ src/components/movies/SortToggle.vue
@@ -0,0 +1,40 @@
+<template>
+  <div class="sort-toggle">
+    <q-toggle
+      v-model="value"
+      label="Ordenar por popularidad"
+      left-label="Fecha de lanzamiento"
+      right-label="Popularidad"
+      style="width: 100%"
+    />
+  </div>
+</template>
+
+<script>
+export default {
+  name: "SortToggle",
+  props: {
+    modelValue: {
+      type: Boolean,
+      default: true, // Ordenar por popularidad como predeterminado
+    },
+  },
+  emits: ["update:modelValue"],
+  computed: {
+    value: {
+      get() {
+        return this.modelValue;
+      },
+      set(value) {
+        this.$emit("update:modelValue", value);
+      },
+    },
+  },
+};
+</script>
+
+<style scoped>
+.sort-toggle {
+  width: 100%;
+}
+</style>
